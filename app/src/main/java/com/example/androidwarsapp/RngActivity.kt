aad45700ddfaf03e5df044aa0d77e567274ec87b
marinslogar <marin.slogar@gmail.com>
2020-09-10 22:00:23 +0200

    Validation screen: 1.send string to rng screen 2.display number from rng screen RNG screen: 1.generate random number 2.send number to validation screen

--- app/src/main/java/com/example/androidwarsapp/RngActivity.kt
+++ app/src/main/java/com/example/androidwarsapp/RngActivity.kt
@@ -1,12 +1,34 @@
 package com.example.androidwarsapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_rng.*
 
 class RngActivity : AppCompatActivity() {
+    var randomNumber : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rng)
         this.title = "RNG screen"
+
+        val stringFromValidation = intent.getStringExtra("validationTextField")
+        rngTextField.text = stringFromValidation
+
+        generateButton.setOnClickListener {
+            randomNumber = generateRandomNumber()
+        }
+
+        closeButton.setOnClickListener {
+            val intent = Intent(this, ValidationActivity::class.java).apply {
+                putExtra("randomNumber", randomNumber)
+            }
+            startActivity(intent)
+        }
+    }
+
+    fun generateRandomNumber(): Int {
+        return (10..99).random()
     }
 }
--- app/src/main/java/com/example/androidwarsapp/ValidationActivity.kt
+++ app/src/main/java/com/example/androidwarsapp/ValidationActivity.kt
@@ -1,12 +1,33 @@
 package com.example.androidwarsapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_validation.*
 
 class ValidationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_validation)
         this.title = "Validation screen"
+
+//        languageButton.setOnClickListener {
+//
+//        }
+//
+//        loginButton.setOnClickListener {
+//
+//        }
+
+        rngButton.setOnClickListener {
+            println(validationTextField.text)
+            val intent = Intent(this, RngActivity::class.java).apply {
+                putExtra("validationTextField", validationTextField.text.toString())
+            }
+            startActivity(intent)
+        }
+
+        val randomNumber = intent.getIntExtra("randomNumber", 0)
+        validationTextField.setText(randomNumber.toString())
     }
 }
